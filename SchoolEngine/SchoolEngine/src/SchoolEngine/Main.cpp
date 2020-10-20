cd9566b042a2e5a4d07ae1346b5ba723d7ffdf14
felix <felix.lundh@gmail.com>
2020-10-20 16:09:05 +0200

    fix

--- SchoolEngine/SchoolEngine/src/SchoolEngine/Main.cpp
+++ SchoolEngine/SchoolEngine/src/SchoolEngine/Main.cpp
@@ -1,10 +1,15 @@
+#include "core/Application.h"
 #include <stdio.h>
 #include <SDL.h>
 #include <iostream>
 
+// The entry point for the application
 int main(int argc, char* argv[]) {
 
-	std::cout << "Hello world" << std::endl;
+	std::cout << "Start..." << std::endl;
+	SchoolEngine::Application app; // Instanciates an application
+	app.run(); // Runds the application
 
+	std::cin.get();
 	return 0;
 }
--- SchoolEngine/SchoolEngine/src/SchoolEngine/core/Window.cpp
+++ SchoolEngine/SchoolEngine/src/SchoolEngine/core/Window.cpp
@@ -4,9 +4,63 @@
 
 namespace SchoolEngine {
 	Window::Window(const char* title, int w, int h) :_window(NULL), _renderer(NULL) {
-		SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, SDL_WINDOW_SHOWN);
+		_window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, SDL_WINDOW_SHOWN);
 
-		if (_window == NULL)
+		if (_window == NULL) {
 			std::cout << "Window failed to init. Error: " << SDL_GetError() << std::endl;
+		}
+		else {
+			std::cout << "Window created!" << std::endl;
+		}
+
+
+		_renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_ACCELERATED);
+
+		if (_renderer == NULL) {
+			std::cout << "Renderer failed to init. Error: " << SDL_GetError() << std::endl;
+		}
+		else {
+			std::cout << "Renderer created!" << std::endl;
+		}
+	}
+
+	SDL_Texture* Window::loadTexture(const char* filePath) {
+		SDL_Texture* texture = NULL;
+		texture = IMG_LoadTexture(_renderer, filePath);
+
+		if (texture == NULL) {
+			std::cout << "Failed to load texture. Error: " << SDL_GetError() << std::endl;
+		}
+
+		return texture;
+	}
+
+	void Window::render(SDL_Texture* tex, unsigned short  int sizeX, unsigned short  int sizeY, unsigned short  int posX, unsigned short  int posY) {
+		SDL_Rect src;
+		src.x = 0;
+		src.y = 0;
+		src.w = sizeX;
+		src.h = sizeY;
+
+		SDL_Rect dst;
+		dst.x = posX;
+		dst.y = posY;
+		dst.w = src.w;
+		dst.h = src.h;
+
+		SDL_RenderCopy(_renderer, tex, &src, &dst);
+	}
+
+	void Window::display() {
+		SDL_RenderPresent(_renderer);
+	}
+
+	void Window::clear() {
+		SDL_RenderClear(_renderer);
+	}
+
+	void Window::cleanUp() {
+		SDL_DestroyWindow(_window);
+		std::cout << "Window destroyed!" << std::endl;
 	}
 }
